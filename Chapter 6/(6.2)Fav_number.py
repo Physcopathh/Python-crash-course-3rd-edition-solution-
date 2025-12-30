2c13b34ee88f340a9ab3d345397d7bd928251280
Asmar Ali <asmarali43210@gmail.com>
2025-12-30 09:45:06 +0500

    Add Chapter 6 solutions

--- Chapter 6/(6.2)Fav_number.py
+++
@@ -1,6 +0,0 @@
-peoples = {'Yousra':9,'Taimoor':9,'Asmar':7,'Laiba':5,'Fazle Shahil':10}
-print('Yousra',peoples['Yousra'])
-print('Taimoor',peoples['Taimoor'])
-print('Asmar',peoples['Asmar'])
-print('Laiba',peoples['Laiba'])
-print('Fazle Shahil',peoples['Fazle Shahil'])
