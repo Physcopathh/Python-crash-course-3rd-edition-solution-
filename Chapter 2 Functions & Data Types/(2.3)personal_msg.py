5b0862808cf985ece9df914a54aa668c0744ec61
Asmar Ali <asmarali43210@gmail.com>
2025-12-30 09:50:20 +0500

    Add Chapter 2: Functions & Data Types solutions

---
+++ Chapter 2 Functions & Data Types/(2.3)personal_msg.py
@@ -0,0 +1,2 @@
+name = "Asmar"
+print(f"“Hello {name}, would you like to learn some Python today?”")
---
+++ Chapter 2 Functions & Data Types/(2.7)spritting_name.py
@@ -0,0 +1,6 @@
+name = "  \n\tAsmar    "
+print(name)
+print(name.lstrip())
+print(name.rsplit())
+print(name.split())
+
